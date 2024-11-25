938f5e690efdc8f9a8c90c38027b01424aa3a2bb
marc <2005marcmc@gmail.com>
2024-11-25 17:49:17 +0100

    Initial Commit

---
+++ src/prova2/Salutacio.java
@@ -0,0 +1,23 @@
+package prova2;
+
+import java.util.Scanner;
+
+public class Salutacio {
+
+	public static void main(String[] args) {
+
+		int i=5;
+		int aleatori = (int) Math.random()+8;
+		
+		Scanner in = new Scanner(System.in);
+		
+		for (int j =1; j<4; j++) {
+			System.out.println("Hola món");
+		}
+		System.out.println(i);
+		System.out.println(aleatori);
+		
+		in.close();
+	}
+
+}
